53cf943ca9d3bf6cfa576ced0f8a9c6599e53a13
Arnav Guneta <arnavguneta@gmail.com>
2023-04-02 16:00:41 -0400

    Modify app settings and set up migrations

--- PieShop/Program.cs
+++ PieShop/Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using PieShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<PieShopDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:PieShopDbContextConnection"]);
+});
 
 var app = builder.Build();
 app.UseStaticFiles();
@@ -17,4 +22,5 @@ if (app.Environment.IsDevelopment())
 
 app.MapDefaultControllerRoute();
 
+DbInitializer.Seed(app);
 app.Run();
