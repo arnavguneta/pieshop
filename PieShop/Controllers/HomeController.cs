e513889d69982b4f4bdc622324b3d749e0432316
Arnav Guneta <arnavguneta@gmail.com>
2023-04-02 18:21:40 -0400

    Set up home controller

---
+++ PieShop/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
+
+namespace PieShop.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly IPieRepository _pieRepository;
+
+		public HomeController(IPieRepository ipieRepository)
+		{
+			_pieRepository = ipieRepository;
+		}
+
+		public IActionResult Index()
+		{
+			var piesOfTheWeek = _pieRepository.PiesOfTheWeek;
+			var homeViewModel = new HomeViewModel(piesOfTheWeek);
+			return View(homeViewModel);
+		}
+	}
+}
--- PieShop/Controllers/PieController.cs
+++ PieShop/Controllers/PieController.cs
@@ -17,8 +17,16 @@ namespace PieShop.Controllers
 
         public IActionResult List()
         {
-            PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese Cakes");
+            PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "All pies");
             return View(piesListViewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
         }
     }
 }
