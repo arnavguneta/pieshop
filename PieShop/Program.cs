2be62c59929f57786932b69a4014b59b63e343bf
Arnav Guneta <arnavguneta@gmail.com>
2023-04-05 15:32:57 -0400

    Setup shopping carts and category menus

--- PieShop/Program.cs
+++ PieShop/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
 
+builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<PieShopDbContext>(options =>
 {
@@ -14,6 +18,7 @@ builder.Services.AddDbContext<PieShopDbContext>(options =>
 
 var app = builder.Build();
 app.UseStaticFiles();
+app.UseSession();
 
 if (app.Environment.IsDevelopment())
 {
